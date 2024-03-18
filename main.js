31769618c39a007a253797cfee24389c38098945
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2024-03-18 13:58:31 -0600

    walls updated

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const width = window.innerWidth, height = window.innerHeight;
 
 // init
 
-const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-camera.position.z = 1;
+const camera = new THREE.PerspectiveCamera( 90, width / height, 0.01, 1000 );
+camera.position.z = 10;
 
 const scene = new THREE.Scene();
 
@@ -22,7 +22,15 @@ renderer.setSize( width, height );
 renderer.setAnimationLoop( animation );
 document.body.appendChild( renderer.domElement );
 
+//lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
+ambientLight.position.z = camera.position;
+scene.add( ambientLight );
 
+
+const sunLight = new THREE.DirectionalLight(0xdddddd, 1.0);
+// sunLight.position.y = 1
+scene.add( sunLight );
 //controls
 document.addEventListener( 'keydown', onKeyDown, false );
 
@@ -30,15 +38,88 @@ document.addEventListener( 'keydown', onKeyDown, false );
 const floortexture = new THREE.TextureLoader().load('image/Floor.jpg');
 floortexture.wrapS = THREE.RepeatWrapping;
 floortexture.wrapT = THREE.RepeatWrapping
-floortexture.repeat.set(4, 4);
+floortexture.repeat.set(5, 5);
 
 //create the floor
-const planegeometry = new THREE.PlaneGeometry( 50, 50);
+const planegeometry = new THREE.PlaneGeometry( 15, 20);
 const planematerial = new THREE.MeshBasicMaterial({ map: floortexture, side: THREE.DoubleSide});
 const plane = new THREE.Mesh( planegeometry, planematerial);
+plane.rotation.x = Math.PI / 2;
+plane.position.y = -Math.PI;
+plane.position.z = -7;
 scene.add( plane );
 
 
+//create the walls
+const wallGroup = new THREE.Group();
+scene.add( wallGroup );
+
+//front wall
+
+const frontwalltexture = new THREE.TextureLoader().load('image/frontwall.jpg');
+frontwalltexture.wrapS = THREE.RepeatWrapping;
+frontwalltexture.wrapT = THREE.RepeatWrapping
+frontwalltexture.repeat.set(1, 0.8);
+
+const frontWall = new THREE.Mesh(
+	new THREE.BoxGeometry(15,6,0.001),
+	new THREE.MeshBasicMaterial({map: frontwalltexture})
+);
+frontWall.position.z = -15;
+
+//right wall
+const rightwalltexture = new THREE.TextureLoader().load('image/wls2.jpg');
+rightwalltexture.wrapS = THREE.RepeatWrapping;
+rightwalltexture.wrapT = THREE.RepeatWrapping
+rightwalltexture.repeat.set(5, 1);
+
+const rightWall = new THREE.Mesh(
+	new THREE.BoxGeometry(19,6,1),
+	new THREE.MeshBasicMaterial({map: rightwalltexture, side: THREE.DoubleSide})
+);
+rightWall.rotation.y = Math.PI / 2;
+rightWall.position.x = -7
+rightWall.position.y = 0
+rightWall.position.z = -6.5
+
+//left wall
+const leftwalltexture = new THREE.TextureLoader().load('image/wls2.jpg');
+leftwalltexture.wrapS = THREE.RepeatWrapping;
+leftwalltexture.wrapT = THREE.RepeatWrapping
+leftwalltexture.repeat.set(4, 1);
+
+const leftWall = new THREE.Mesh(
+	new THREE.BoxGeometry(19,6,1),
+	new THREE.MeshBasicMaterial({ map: leftwalltexture, side: THREE.DoubleSide})
+
+);
+leftWall.rotation.y = Math.PI / 2;
+leftWall.position.x = 7
+leftWall.position.y = 0
+leftWall.position.z = -6.5
+
+//ceiling
+const ceilingtexture = new THREE.TextureLoader().load('image/ceiling.jpg');
+ceilingtexture.wrapS = THREE.RepeatWrapping;
+ceilingtexture.wrapT = THREE.RepeatWrapping
+ceilingtexture.repeat.set(1, 0.5);
+
+const ceiling = new THREE.Mesh(
+	new THREE.BoxGeometry(15,20,0.5),
+	new THREE.MeshBasicMaterial({ map: ceilingtexture, side: THREE.DoubleSide})
+);
+ceiling.rotation.x = Math.PI / 2;
+ceiling.position.y = Math.PI;
+ceiling.position.z = -7;
+scene.add(ceiling);
+
+
+wallGroup.add( frontWall, rightWall,leftWall );
+//loop through each wall an create the bounding box
+for (let i = 0; i < wallGroup.children.length; i++) {
+	wallGroup.children[i].BBox = new THREE.Box3();
+	wallGroup.children[i].BBox.setFromObject(wallGroup.children[i]);
+}
 
 // animation
 
@@ -57,19 +138,19 @@ function onKeyDown(event) {
 	let keycode = event.which;
 	// right arrow
 	if ( keycode === 39) {
-		camera.translateX(-0.05)
+		camera.translateX(0.05)
 	}
 	// left arrow
 	else if ( keycode === 37){
-		camera.translateX(0.05)
+		camera.translateX(-0.05)
 	}
 	//up arrow
 	else if ( keycode === 38) {
-		camera.translateY(-0.05)
+		camera.translateZ(-0.05)
 	}
 	//down arrow
 	else if ( keycode === 40) {
-		camera.translateY(0.05)
+		camera.translateZ(0.05)
 	}
 
 }
